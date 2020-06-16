dd4ab8b60b5899c9c29d0b3fd34c8e166aad1f19
lorenzoKrol <42969133+lorenzoKrol@users.noreply.github.com>
2020-06-16 11:05:57 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-  define ('DB_HOST','localhost');
-  define ('DB_USER','s629808_LK629808');
-  define ('DB_PASSWORD','Mienoe2008');
-  define ('DB_NAME','s629808_dbphp');
+  define ('DB_HOST','//hostname');
+  define ('DB_USER','//username');
+  define ('DB_PASSWORD','//password');
+  define ('DB_NAME','//database name');
   $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
 
